be298dd02254b84926951fdf57ddf04cc8134afb
Alexey Drumlevich <l-gc@yandex.ru>
2023-08-20 21:01:36 +0300

    Fix ViewTestAppl and SimpleCalculatorAppl

--- Employees/src/telran/view/Menu.java
+++ Employees/src/telran/view/Menu.java
@@ -2,19 +2,12 @@ package telran.view;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Stack;
-import java.util.UUID;
 import java.util.stream.IntStream;
 
 public class Menu implements Item {
 	private static final int N_SYMBOLS = 20;
-	
-	private Stack<Menu> stackMenu = new Stack<>();
-	
 	String name;
 	ArrayList<Item> items;
-	private String id;
-	
 	public Menu(String name, ArrayList<Item> items) {
 		this.name = name;
 		this.items = items;
@@ -22,8 +15,6 @@ public class Menu implements Item {
 	public Menu (String name, Item ...items) {
 		this(name, new ArrayList<>(Arrays.asList(items)));
 	}
-	
-	
 	@Override
 	public String displayName() {
 		
@@ -33,8 +24,6 @@ public class Menu implements Item {
 	@Override
 	public void perform(InputOutput io) {
 		boolean running = true;
-		Menu subMenu = null;
-		
 		do {
 			try {
 				displayTitle(io);
@@ -42,30 +31,13 @@ public class Menu implements Item {
 				int itemNumber = io.readInt("Enter item number", "Wrong item number",
 						1, items.size());
 				Item item = items.get(itemNumber - 1);
-				
-				//SUB MENU
-				if(item instanceof Menu) {
-					if(this.id == null) {
-						id = UUID.randomUUID().toString();
-						MenuNavigator.push(this);
-					}				
-					subMenu = (Menu) item;
-					subMenu.setId(id);
-					MenuNavigator.push(subMenu);
-				}
-				
 				item.perform(io);
 				running = !item.isExit();
 			} catch (Throwable e) {
 				io.writeLine(e.getMessage());
 			}
 		} while(running);
-		
-		
-		if(!MenuNavigator.isEmpty(this)) {
-			MenuNavigator.pop(this);
-			MenuNavigator.peek(this).perform(io);
-		}
+
 	}
 
 	private void displayItemNames(InputOutput io) {
@@ -83,14 +55,6 @@ public class Menu implements Item {
 	public boolean isExit() {
 		
 		return false;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
 	}
 
 }
