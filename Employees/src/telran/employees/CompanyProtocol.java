b958410dbeac15fdcb89a6b8d2ba6cc496de6dd5
Alexey Drumlevich <l-gc@yandex.ru>
2023-10-19 16:56:49 +0300

    company protocol fix

--- Employees/src/telran/employees/CompanyProtocol.java
+++ Employees/src/telran/employees/CompanyProtocol.java
@@ -2,13 +2,6 @@ package telran.employees;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 import telran.employees.dto.Employee;
 import telran.employees.dto.FromTo;
@@ -27,34 +20,6 @@ public class CompanyProtocol implements ApplProtocol {
 		this.company = company;
 	}
 
-	ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock(); 
-	Lock readLock = reentrantReadWriteLock.readLock();
-	Lock writeLock = reentrantReadWriteLock.writeLock();
-	private Function<Serializable, Serializable> readLockSupplier = new Function<Serializable, Serializable>() {
-		@Override
-		public Serializable apply(Serializable t) {
-			readLock.lock();
-			try {
-				return t;
-			} finally {
-				readLock.unlock();
-			}
-		}
-	};
-	
-	private Function<Serializable, Serializable> writeLockSupplier = new Function<Serializable, Serializable>() {
-		@Override
-		public Serializable apply(Serializable t) {
-			writeLock.lock();
-			try {
-				return t;
-			} finally {
-				writeLock.unlock();
-			}
-		}
-	};
-	
-	
 	@Override
 	public Response getResponse(Request request) {
 		Response response = null;
@@ -62,18 +27,19 @@ public class CompanyProtocol implements ApplProtocol {
 		Serializable data = request.requestData();
 		try {
 			Serializable responseData = switch(requestType) {
-			case "employee/add" -> writeLockSupplier.apply(employee_add(data));
-			case "employee/get" ->  readLockSupplier.apply(employee_get(data));
-			case "employees/get" -> readLockSupplier.apply(employees_get(data));
-			case "employees/department" -> readLockSupplier.apply(employees_department(data));
-			case "employees/salary" -> readLockSupplier.apply(employees_salary(data));
-			case "employees/age" -> readLockSupplier.apply(employees_age(data));
-			case "department/update" ->  writeLockSupplier.apply(department_update(data));
-			case "salary/update" -> writeLockSupplier.apply(salary_update(data));
-			case "employee/remove" -> writeLockSupplier.apply(employee_remove(data));
-			case "department/salary/distribution" -> readLockSupplier.apply(department_salary_distribution(data));
-			case "salary/distribution" -> readLockSupplier.apply(salary_distribution(data));
-			default -> new Response(ResponseCode.WRONG_TYPE, requestType +
+			case "employee/add" -> employee_add(data);
+			case "employee/get" -> employee_get(data);
+			case "employees/get" -> employees_get(data);
+			case "department/update" -> department_update(data);
+			case "employee/remove" -> employee_remove(data);
+			case "department/salary/distribution" ->
+			department_salary_distribution(data);
+			case "salary/distribution" -> salary_distribution(data);
+			case "employees/department" -> employees_department(data);
+			case "employees/salary" -> employees_salary(data);
+			case "employees/age" -> employees_age(data);
+			case "salary/update" -> salary_update(data);
+			    default -> new Response(ResponseCode.WRONG_TYPE, requestType +
 			    		" is unsupported in the Company Protocol");
 			};
 			response = (responseData instanceof Response) ? 
@@ -85,50 +51,51 @@ public class CompanyProtocol implements ApplProtocol {
 		}
 		return response;
 	}
-	
+
 	 private Serializable salary_update(Serializable data) {
-		@SuppressWarnings("unchecked")
-		UpdateData<Integer> updateData = (UpdateData<Integer>) data;
-		long id = updateData.id();
-		int newSalary = updateData.data();
-		return company.updateSalary(id, newSalary);
+		 @SuppressWarnings("unchecked")
+			UpdateData<Integer> updateData = (UpdateData<Integer>) data;
+			long id = updateData.id();
+			int salary = updateData.data();
+			return company.updateSalary(id, salary);
 	}
-	
+
 	private Serializable employees_age(Serializable data) {
 		FromTo fromTo = (FromTo) data;
-		return new ArrayList<> (company.getEmployeesByAge(fromTo.from(), fromTo.to()));
+		int ageFrom = fromTo.from();
+		int ageTo = fromTo.to();
+		return new ArrayList<>(company.getEmployeesByAge(ageFrom, ageTo));
 	}
-	
+
 	private Serializable employees_salary(Serializable data) {
 		FromTo fromTo = (FromTo) data;
-		return new ArrayList<> (company.getEmployeesBySalary(fromTo.from(), fromTo.to()));
+		int salaryFrom = fromTo.from();
+		int salaryTo = fromTo.to();
+		return new ArrayList<>(company.getEmployeesBySalary(salaryFrom, salaryTo));
 	}
-	
+
 	private Serializable employees_department(Serializable data) {
-		String department = (String) data;
-		return new ArrayList<> (company.getEmployeesByDepartment(department));
+		String department = (String)data;
+		return new ArrayList<>(company.getEmployeesByDepartment(department ));
 	}
-	
-	
+
 	private Serializable salary_distribution(Serializable data) {
 		int interval = (int) data;
-		return new ArrayList<> (company.getSalaryDistribution(interval));
+		return new ArrayList<>(company.getSalaryDistribution(interval));
 	}
-	
+
 	private Serializable department_salary_distribution(Serializable data) {
-		Serializable a = (Serializable) company.getDepartmentSalaryDistribution();
-		List<Employee> b = (List<Employee>) a;
-		System.out.println((a == b) ? "==" : "!=");
-		
-		return new ArrayList<> (company.getDepartmentSalaryDistribution());
+		return new ArrayList<>(company.getDepartmentSalaryDistribution());
 	}
-	
+
 	private Serializable employee_remove(Serializable data) {
-		long id = (long) data;
+		long id = (long)data;
+		
 		return company.removeEmployee(id);
 	}
-	
-	 private Serializable department_update(Serializable data) {
+
+	private Serializable department_update(Serializable data) {
+		
 		@SuppressWarnings("unchecked")
 		UpdateData<String> updateData = (UpdateData<String>) data;
 		long id = updateData.id();
