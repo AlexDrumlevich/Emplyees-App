bd8b97ce833f697161e03e15b95d983663024fd4
Alexey Drumlevich <l-gc@yandex.ru>
2023-10-19 16:33:01 +0300

    reconnect fix

--- Employees/src/telran/net/TcpHandler.java
+++ Employees/src/telran/net/TcpHandler.java
@@ -3,17 +3,21 @@ package telran.net;
 import java.io.*;
 import java.net.*;
 public class TcpHandler implements Closeable{
+	private String host;
+	private int port;
     private Socket socket;
     private ObjectOutputStream output;
     private ObjectInputStream input;
-    private String host;
-    private int port;
-    
     public TcpHandler(String host, int port) throws Exception {
     	this.host = host;
     	this.port = port;
     	connect();
     }
+	private void connect() throws UnknownHostException, IOException {
+		socket = new Socket(host, port);
+    	output = new ObjectOutputStream(socket.getOutputStream());
+    	input = new ObjectInputStream(socket.getInputStream());
+	}
 	@Override
 	public void close() throws IOException {
 		socket.close();
@@ -21,54 +25,35 @@ public class TcpHandler implements Closeable{
 	}
 	public <T> T send(String requestType, Serializable requestData)  {
 		Request request = new Request(requestType, requestData);
-		T res = null;
-		try {
-			output.writeObject(request);
-			res = getResponse(input);
-			if(res == null) {
-				connect();
-				output.writeObject(request);
-				res = getResponse(input);
-			}
-		} catch (IOException e) {
-			try {
-				connect();
-				output.writeObject(request);
-				res = getResponse(input);
-			} catch (Exception e1) {
-				
-				e1.printStackTrace();
-			}
-			
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		}
-		return res;
+		boolean running = true;
 		
-	
-	}
-	
-	private <T> T getResponse(ObjectInputStream input) {
-		try {
-			Response response = (Response) input.readObject();
-			if (response.code() != ResponseCode.OK) {
-				throw new RuntimeException(response.responseData().toString());
-			}
-			@SuppressWarnings("unchecked")
-			T res = (T) response.responseData();
-			return res;
-		} catch (Exception e) {
-			
-			throw new RuntimeException(e.getMessage());
+		while (running) {
+			running = false;
+			try {
+				output.writeObject(request);
+				Response response = (Response) input.readObject();
+				if (response.code() != ResponseCode.OK) {
+					throw new RuntimeException(response.responseData().toString());
+				}
+				@SuppressWarnings("unchecked")
+				T res = (T) response.responseData();
+				return res;
+				
+			} catch (Exception e) {
+				if(e instanceof SocketException) {
+					running = true;
+					try {
+						connect();
+					} catch (Exception e1) {
+						
+					} 
+				} else {
+					throw new RuntimeException(e.getMessage());
+				}
+				
+			} 
 		}
+		return null;
 	}
-	
-	private void connect() throws Exception {
-		System.out.println("connection");
-		socket = new Socket(host, port);
-    	output = new ObjectOutputStream(socket.getOutputStream());
-    	input = new ObjectInputStream(socket.getInputStream());
-	}
-	
 
 }
